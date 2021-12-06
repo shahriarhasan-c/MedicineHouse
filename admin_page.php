8f1a01b4055cfc19a3c227bbe3322202eb227b43
Md Sakib Hossain <hossainsakib344@gmail.com>
2021-12-06 23:24:11 +0600

    admin page added

---
+++ admin_page.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <title>Medicine House</title>
+    <link href="admin_page_css.css" rel="stylesheet" type="text/css">
+</head> 
+<body>
+<div class="logout" style="margin-left: : 10px;">
+<a href="home_page.php"><input type="button" name="logout" value="Logout"></a>
+</div>
+    <header>
+        
+    <div class="row">
+        <div class="logo">
+        </div>   
+    </div>
+        
+    <div class="hero">
+    <h1>Do You Want To Change ?</h1>
+    <div class="button">
+        <a href="admin_insert.php" style="text-decoration: none;"><h2 class="btn btn-one">Insert Medicine</h2></a>
+        <a href="delete_records.php" style="text-decoration: none;"> <h2 class="btn btn-two">Delete Medicine</h2>
+        <a href="update_rec.php" style="text-decoration: none;"><h2 class="btn btn-three">Update Medicine</h2>
+    </div>    
+            
+    </div>
+    
+    </header>
+
+
+        
+</body>    
+</html>
