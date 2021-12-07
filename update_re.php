8d3e44b685ab1ac3dcdec3593184e68ce17f46d5
Md. Anisur Rahman <mrahman191191@bscse.uiu.ac.bd>
2021-12-07 07:52:49 +0600

    for updating the changes to the database a new file is added

---
+++ update_re.php
@@ -0,0 +1,24 @@
+
+<?php
+       $con = mysqli_connect("localhost", "root", "", "project");
+       $update =  "UPDATE medicine_list SET pname = '$_POST[name]',price = '$_POST[price]' WHERE id = '$_POST[id]' ";
+       if (mysqli_query($con,$update )) {
+       	  
+       	  header("refresh:1; url:update_rec.php");
+       	  echo "
+
+          				    <script>
+
+                                  alert ('Records updated:');
+                                  window.location.href='update_rec.php';
+
+          				    </script>
+
+          				";
+          				 
+       }
+       else
+       {
+       	echo "sorry";
+       }
+?>
