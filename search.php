96401ca1693cb9d47a9263b55a969964f648e843
S. M. Fahim Haque Nabil <fahim.nabil.h@gmail.com>
2021-12-07 19:04:06 +0600

    Search Option Added

---
+++ search.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Shopping Cart</title>
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Titillium+Web');
+        *{
+            font-family: 'Titillium Web', sans-serif;
+        }
+        .product{
+            border: 1px solid #eaeaec;
+            margin: -1px 19px 3px -1px;
+            padding: 10px;
+            text-align: center;
+            background-color: #efefef;
+        }
+        table, th, tr{
+            text-align: center;
+        }
+        .title2{
+            text-align: center;
+            color: #66afe9;
+            background-color: #efefef;
+            padding: 2%;
+        }
+        h2{
+            text-align: center;
+            color: #66afe9;
+            background-color: #efefef;
+            padding: 2%;
+        }
+         h1{
+            text-align: center;
+            color: #66afe9;
+            background-color: #efefef;
+            padding: 2%;
+            margin-top: 35%;
+        }
+        table th{
+            background-color: #efefef;
+        }
+        body
+        {
+          margin: 0;
+          padding: 0;
+          font-family: sans-serif;
+          background: url();
+          background-size: cover;
+        }
+       
+    </style>
+</head>
+<body>
+<?php
+         $database_name = "project";
+         $con = mysqli_connect("localhost","root","",$database_name);
+        if (isset($_POST['submit'])) {
+            $fet= $_POST['valueToSearch'];
+            $query = "SELECT * FROM medicine_list WHERE medicine_list.pname LIKE '%$fet%' ";
+            $result = mysqli_query($con,$query);
+            if(mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_array($result)) {
+                    ?>
+                    <div class="col-md-2">
+
+                        <form method="post" action="add_to_cart.php?action=add&id=<?php echo $row["id"]; ?>">
+
+                            <div class="product">
+                                <img src="<?php echo $row["image"]; ?>" class="img-responsive">
+                                <h5 class="text-info"><?php echo $row["pname"]; ?></h5>
+                                <h5 class="text-danger"><?php echo $row["price"]; ?></h5>
+                                <input type="text" name="quantity" class="form-control" value="1">
+                                <input type="hidden" name="hidden_name" value="<?php echo $row["pname"]; ?>">
+                                <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>">
+                                <input type="submit" name="add" style="margin-top: 5px;" class="btn btn-success"
+                                       value="Add to Cart">
+                            </div>
+                        </form>
+                    </div>
+                    <?php
+                }
+            }
+        }
+        else
+        {
+                    echo "
+
+                      <script>
+
+                                  alert ('Back:');
+                                  window.location.href='shopping_cart.php';
+
+                      </script>
+
+                  ";
+        }
+
+ ?>
+ 
+</body>
+</html>
