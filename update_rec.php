7b247e19f002eba03051c250d928bdd3f2cfe736
Md. Anisur Rahman <mrahman191191@bscse.uiu.ac.bd>
2021-12-07 07:30:44 +0600

    A background is added in update feature

--- update_rec.php
+++ update_rec.php
@@ -6,7 +6,9 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     
         <style type="text/css">
-            
+            body {
+                background-image: url("a.jpg");
+            }
 			.th
 			{   
 				color: orange;
