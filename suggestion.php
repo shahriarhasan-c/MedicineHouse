6bc8502792b209d2ebffa2ad0ba9ecf7ffa55bef
Md. Anisur Rahman <mrahman191191@bscse.uiu.ac.bd>
2022-01-11 12:59:38 +0600

    a new file inserted named suggestion.php to share the patients problem

---
+++ suggestion.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Get Suggestion</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="login_css.css">
+</head>
+<body>
+<form action="suggestion_DATABASE.php" method="POST">
+	<div class="box">
+	<h2>Share Problem</h2>
+	<form >
+		<div class="inputBox">
+						<input type="text" name="patient" required >
+						<label>Enter Patient Name</label>
+					</div>
+					<div class="inputBox">
+						<input type="text" name="age" required>
+						<label>Age</label>
+					</div>
+					<div class="inputBox">
+						<input type="text" name="email" required>
+						<label>Email</label>
+					</div>
+					<div class="inputBox">
+						<input type="text" name="disease" required>
+						<label>Other Diseases?</label>
+					</div>
+					<div class="inputBox">
+						<input type="text" name="description" required>
+						<label>Describe Your Problem</label>
+					</div>
+		<input type="submit" name="sub" value="Submit">
+		<a href="feedback_input.php"><input style="background-color: green;" type="button" name="" value="See Feedback"></a>
+	</form>
+</div>
+</form>
+</body>
+</html>
